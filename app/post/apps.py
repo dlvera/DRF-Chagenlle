0a64c59512bf6632a84207601d6f5c1b4aa850f5
dlvera <dlvera604@gmail.com>
2025-08-21 14:47:56 +0200

    mODELOS

--- app/post/apps.py
+++ app/post/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.post'
-    #label = 'app_post'  # Opcional pero recomendado para evitar conflictos
+    label = 'app_post'  # Opcional pero recomendado para evitar conflictos
